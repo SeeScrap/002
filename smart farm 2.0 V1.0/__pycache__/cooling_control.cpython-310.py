6ed3e8a0298137ddc6f2939012287cfda1d7b2cc
SeeScrap <136986874+SeeScrap@users.noreply.github.com>
2025-07-11 12:54:14 +0700

    Delete smart farm 2.0 V1.0 directory

--- smart farm 2.0 V1.0/__pycache__/cooling_control.cpython-310.py
+++
@@ -1,6 +0,0 @@
-# uncompyle6 version 3.9.2
-# Python bytecode version base 3.10.0 (3439)
-# Decompiled from: Python 3.10.11 (tags/v3.10.11:7d4cc5a, Apr  5 2023, 00:38:17) [MSC v.1929 64 bit (AMD64)]
-# Embedded file name: c:\Users\User\Desktop\smart farm 2.0 V1.0\cooling_control.py
-# Compiled at: 2025-07-09 17:58:52
-# Size of source mod 2**32: 821 bytes
